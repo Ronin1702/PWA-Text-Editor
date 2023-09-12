e46ac0c92d31ec14cb5040419767c7178e895283
Ronin1702 <kchen@me.com>
2023-09-12 12:55:58 -0400

    database.js updated, IndexedDB shows and stores inputs in DevTool

--- server/server.js
+++ server/server.js
@@ -9,4 +9,4 @@ app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+app.listen(PORT, () => console.log("Check out the app locally @ "+`http://localhost:${PORT}`+" 🚀!"));
