ae7c470ca21db8a10e405cdd89532dd4964c1ece
Ronin1702 <kchen@me.com>
2023-09-12 05:23:15 -0400

    configured webpack and rejistered SW

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,19 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+// Register route for caching images
+registerRoute(
+  ({ request }) => request.destination === 'page',
+  new CacheFirst({
+    cacheName: 'page-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
