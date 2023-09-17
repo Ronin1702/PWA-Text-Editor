86b3c5504f72df765f1e819b1a4f9dc16930dda1
Ronin1702 <kchen@me.com>
2023-09-17 19:24:56 -0400

    testing for prompt rest

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -23,8 +23,8 @@ butInstall.addEventListener('click', async () => {
     // Show prompt
     promptEvent.prompt();
     
-    // Reset the deferred prompt variable, it can only be used once.
-    window.deferredPrompt = null;
+    // // Reset the deferred prompt variable, it can only be used once.
+    // window.deferredPrompt = null;
     
     butInstall.classList.toggle('hidden', true);
 });
